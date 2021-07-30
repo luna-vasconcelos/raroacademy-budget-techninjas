c57b9b1f56048c17f4ee1133a141b5d547bb20cd
LetRicette <leticiaricette@id.uff.br>
2021-07-30 14:30:17 -0300

    shared - const color

---
+++ lib/shared/app_constants/app_colors.dart
@@ -0,0 +1,26 @@
+import 'dart:math';
+
+import 'package:flutter/widgets.dart';
+
+class AppColors {
+  static const Color rosa = Color.fromRGBO(245, 72, 127, 1);
+  static const Color verde = Color.fromRGBO(88, 179, 104, 1);
+  static const Color roxo = Color.fromRGBO(52, 48, 144, 1);
+  static const Color ciano = Color.fromRGBO(68, 194, 253, 1);
+  static const Color amarelo = Color.fromRGBO(250, 199, 54, 1);
+  static const Color background = Color(0xFFE5E5E5);
+
+  static const blueGradient = LinearGradient(
+    colors: <Color>[
+      ciano,
+      roxo,
+    ],
+    begin: Alignment.topLeft,
+    end: Alignment.bottomRight,
+    stops: [
+      0.1,
+      0.7,
+    ],
+    transform: GradientRotation((-34 * pi) / 180),
+  );
+}

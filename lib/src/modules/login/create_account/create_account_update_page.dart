7516250df303afb4c8c07b6e1b9932c786baf364
ronaldofes <ronaldofes@gmail.com>
2021-08-12 02:31:46 +0100

    Ajuste ElevatedButton e Criação da Account Update

---
+++ lib/src/modules/login/create_account/create_account_update_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CreateAccountUpdate extends StatefulWidget {
+  const CreateAccountUpdate({Key? key}) : super(key: key);
+
+  @override
+  _CreateAccountUpdateState createState() => _CreateAccountUpdateState();
+}
+
+class _CreateAccountUpdateState extends State<CreateAccountUpdate> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

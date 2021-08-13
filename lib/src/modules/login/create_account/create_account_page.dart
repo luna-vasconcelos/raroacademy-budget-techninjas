00eddf103dae8c1c9a53f205d821e9a3572770ff
ronaldofes <ronaldofes@gmail.com>
2021-08-13 01:16:57 +0100

    Ajuste do Elevatted Button

--- lib/src/modules/login/create_account/create_account_page.dart
+++ lib/src/modules/login/create_account/create_account_page.dart
@@ -1,9 +1,12 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:raroacademy_budget_techninjas/src/modules/login/create_account/create_account_controller.dart';
+import 'package:raroacademy_budget_techninjas/src/modules/login/create_account/create_account_module.dart';
 import 'package:raroacademy_budget_techninjas/src/modules/login/create_account/create_account_steps/create_account_page_step1.dart';
 import 'package:raroacademy_budget_techninjas/src/modules/login/create_account/create_account_steps/create_account_page_step3.dart';
 import 'package:raroacademy_budget_techninjas/src/modules/login/create_account/create_account_steps/create_account_page_step4.dart';
+import 'package:raroacademy_budget_techninjas/src/modules/login/create_account/create_account_user_model.dart';
 import 'package:raroacademy_budget_techninjas/src/shared/app_constants/text_styles.dart';
 import 'package:raroacademy_budget_techninjas/src/shared/app_widgets/elevated_buttom_widget.dart';
 import 'create_account_steps/create_account_page_step2.dart';
@@ -34,66 +37,76 @@ class _CreateAccountPageState
           CreateAccountPageviewStep4(),
         ],
       ),
-      bottomNavigationBar: AnimatedBuilder(
-        animation: controller.pageViewController,
-        builder: (context, snapshot) {
-          return Container(
-            color: Colors.white,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Container(
-                  width: 128,
-                  height: 48,
-                  child: TextButton.icon(
-                    icon: Icon(
-                      Icons.arrow_back,
-                      color: Color.fromRGBO(0, 0, 0, 0.26),
-                    ),
-                    label: Text(
-                      " VOLTAR",
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontFamily: "Roboto",
-                        fontWeight: FontWeight.w500,
-                        color: Color.fromRGBO(0, 0, 0, 0.26),
-                      ),
-                    ),
-                    onPressed: () {
-                      controller.pageViewController.previousPage(
-                        duration: Duration(milliseconds: 400),
-                        curve: Curves.easeInOut,
-                      );
-                    },
-                  ),
-                ),
-                Text("${currentPage + 1}/4",
-                    style:
-                        TextStyles.createAccountTextStyleBottomNavigatorNumber),
-                Padding(
-                  padding: const EdgeInsets.only(right: 0),
-                  child: ElevatedButtonWidget(
-                      fontSize: 14,
-                      buttonText: "CONTINUAR  ",
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.only(bottom: 8),
+        child: AnimatedBuilder(
+          animation: controller.pageViewController,
+          builder: (context, snapshot) {
+            return Padding(
+              padding: const EdgeInsets.only(left: 12),
+              child: Container(
+                color: Colors.white,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
                       width: 116,
-                      height: 32,
-                      paddingValue: 0,
-                      sufixicon: Icon(
-                        Icons.arrow_forward,
-                        size: 18,
+                      height: 48,
+                      child: TextButton.icon(
+                        icon: Icon(
+                          Icons.arrow_back,
+                          color: Color.fromRGBO(0, 0, 0, 0.26),
+                        ),
+                        label: Text(
+                          "VOLTAR",
+                          style: TextStyle(
+                            fontStyle: FontStyle.normal,
+                            fontSize: 16,
+                            fontFamily: "Roboto",
+                            fontWeight: FontWeight.w500,
+                            letterSpacing: 0.4,
+                            textBaseline: TextBaseline.alphabetic,
+                            color: Color.fromRGBO(0, 0, 0, 0.26),
+                          ),
+                        ),
+                        onPressed: () {
+                          controller.pageViewController.previousPage(
+                            duration: Duration(milliseconds: 400),
+                            curve: Curves.easeInOut,
+                          );
+                        },
                       ),
-                      onpressed: () {
-                        controller.pageViewController.nextPage(
-                          duration: Duration(milliseconds: 400),
-                          curve: Curves.easeInOut,
-                        );
-                      }),
-                )
-              ],
-            ),
-          );
-        },
+                    ),
+                    Text("${currentPage + 1}/4",
+                        style: TextStyles
+                            .createAccountTextStyleBottomNavigatorNumber),
+                    Padding(
+                      padding: const EdgeInsets.only(right: 12),
+                      child: ElevatedButtonWidget(
+                          mainAlign: MainAxisAlignment.spaceBetween,
+                          fontSize: 14,
+                          buttonText: "CONTINUAR ",
+                          width: 116,
+                          height: 32,
+                          paddingValue: 4,
+                          sufixicon: Icon(
+                            Icons.arrow_forward,
+                            size: 18,
+                          ),
+                          onpressed: () {
+                            controller.pageViewController.nextPage(
+                              duration: Duration(milliseconds: 400),
+                              curve: Curves.easeInOut,
+                            );
+                          }),
+                    )
+                  ],
+                ),
+              ),
+            );
+          },
+        ),
       ),
     );
   }

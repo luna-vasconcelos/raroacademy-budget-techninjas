0de74eb2ab19fc1833dbc33625ff24ee4b81de38
Luna Vasconcelos <lunacv94@gmail.com>
2021-08-15 16:15:59 -0300

    appbar do cash flow

--- lib/src/app_module.dart
+++ lib/src/app_module.dart
@@ -1,8 +1,6 @@
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:raroacademy_budget_techninjas/src/app_controller.dart';
 import 'package:raroacademy_budget_techninjas/src/modules/authentication/splash_module.dart';
-import 'package:raroacademy_budget_techninjas/src/modules/home/cash_flow/initial_cash_flow/initial_cash_flow_module.dart';
-import 'package:raroacademy_budget_techninjas/src/modules/home/cash_flow/initial_cash_flow/initial_cash_flow_page.dart';
 import 'package:raroacademy_budget_techninjas/src/modules/home/home/home_module.dart';
 import 'package:raroacademy_budget_techninjas/src/modules/login/create_account/create_account_module.dart';
 import 'package:raroacademy_budget_techninjas/src/modules/login/login_module.dart';
@@ -17,7 +15,7 @@ class AppModule extends Module {
 
   @override
   List<ModularRoute> get routes => [
-        ModuleRoute(Modular.initialRoute, module: CashFlowModule()),
+        ModuleRoute(Modular.initialRoute, module: SplashModule()),
         ModuleRoute("/initial", module: InitialPageModule()),
         ModuleRoute("/login", module: LoginModule()),
         ModuleRoute("/createaccount", module: CreateAccountModule()),

5a7ee18595d963bcb3030955e1cf83f43be46a64
Luna Vasconcelos <lunacv94@gmail.com>
2021-08-17 23:49:42 -0300

    componentização user card

--- lib/src/modules/home/home/home_module.dart
+++ lib/src/modules/home/home/home_module.dart
@@ -1,7 +1,5 @@
 import 'package:flutter_modular/flutter_modular.dart';
-import 'package:raroacademy_budget_techninjas/src/modules/home/cash_flow/cash_flow_controller.dart';
 import 'package:raroacademy_budget_techninjas/src/modules/home/cash_flow/cash_flow_module.dart';
-import 'package:raroacademy_budget_techninjas/src/modules/home/cash_flow/cash_flow_page.dart';
 import 'package:raroacademy_budget_techninjas/src/modules/home/home/home.controller.dart';
 import 'package:raroacademy_budget_techninjas/src/modules/home/home/home_page.dart';
 

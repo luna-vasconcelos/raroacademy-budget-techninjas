95ea4a7a37b75af6d2148ee57e4c34edef35ae79
Luna Vasconcelos <lunacv94@gmail.com>
2021-08-21 16:49:03 -0300

    login ok de verdadeira mesmo

--- lib/src/modules/login/login_repository.dart
+++ lib/src/modules/login/login_repository.dart
@@ -8,27 +8,17 @@ class LoginRepository {
 
   // signup
 
-  //Stream<User?> get authStateChanges => _firebaseAuth.authStateChanges();
 
-  // login
-  /*Future<void> login(String email, String password) async {
-    try {
-      await _firebaseAuth.loginWithEmailAndPassword(
-          email: email, password: password);
-    } on FirebaseAuthException catch (e) {
-      print(e.message);
-    }
-  }*/
-
+  // login-signin
   Future<String?> login(String email, String password) async {
     try {
       await auth.signInWithEmailAndPassword(email: email, password: password);
       return Modular.to.pushReplacementNamed('/home');
     } on FirebaseAuthException catch (e) {
       if (e.code == 'user-not-found') {
-        return 'No user found for that email.';
+        print('Usuário não encontrado.');
       } else if (e.code == 'wrong-password') {
-        return 'Wrong password provided for that user.';
+        print('Senha incorreta.');
       }
       print(e.message);
     }

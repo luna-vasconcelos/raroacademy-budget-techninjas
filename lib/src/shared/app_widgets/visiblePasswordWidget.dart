43999490108ab51454d37f7a264380a8ce96fed9
LetRicette <leticiaricette@id.uff.br>
2021-08-22 22:52:05 -0300

    DropdownMonths and eye on login page

---
+++ lib/src/shared/app_widgets/visiblePasswordWidget.dart
@@ -0,0 +1,21 @@
+// import 'package:flutter/material.dart';
+
+// class ShowPassword extends StatelessWidget {
+//   const ShowPassword({
+//     Key? key,
+//     this.visible = false,
+//     required this.onPressed,
+//   }) : super(key: key);
+
+//   final bool visible;
+//   final VoidCallback onPressed;
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return IconButton(
+//       onPressed: onPressed,
+//       icon: Icon(visible ? Icons.visibility_off : Icons.visibility_rounded,
+//           color: Colors.black),
+//     );
+//   }
+// }
